bcda0ca4956843444b1e250aa8c9b2175dfd4220
Harmon Herring <harmonherring@gmail.com>
2019-12-10 00:14:55 -0500

    Stub log_type route

--- lockheed_141310/routes/log.py
+++ lockheed_141310/routes/log.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request, jsonify
+from flask_jwt_extended import jwt_required
 from sqlalchemy.dialects.postgresql import UUID
 
 from lockheed_141310.models import CMLog
@@ -7,8 +8,25 @@ log_bp = Blueprint('log_bp', __name__)
 
 
 # pylint: disable=inconsistent-return-statements
+@log_bp.route('/<cm_uuid>', methods=['GET', 'POST', 'DELETE'])
+@jwt_required
+def log_type_route(cm_uuid: UUID):
+    """
+    :GET: returns a list of the available log types
+    :POST: adds a log type
+    :DELETE: deletes a log type
+    """
+    if request.method == 'GET':
+        pass
+    if request.method == 'POST':
+        pass
+    if request.method == 'DELETE':
+        pass
+
+
+# pylint: disable=inconsistent-return-statements
 @log_bp.route('/<cm_uuid>/<log_type>', methods=['GET', 'POST'])
-#@jwt_required
+@jwt_required
 def log(cm_uuid: UUID, log_type: str):
     """
     :GET: returns the specified number of logs matching the specified log type
